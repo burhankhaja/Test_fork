87c7880f1ad715fc411e6e00ec34bed6d17d8ebc
Burhan khaja <118989617+burhankhaja@users.noreply.github.com>
2025-07-18 23:50:47 +0530

    Create file.rs

---
+++ file.rs
@@ -0,0 +1,12 @@
+use anchor_lang::prelude::*;
+
+#[account] 
+#[derive(Accounts)]
+pub struct Lending {
+  pub lender: Pubkey,
+  pub borrower: Pubkey,
+  pub amount: u8, //@note :: dummy flaw that will be fixed by deeydos via PR to u64
+  pub interest_bps: u16,
+  pub borrowed_date: i64,
+  pub grace_period: i64,
+}
